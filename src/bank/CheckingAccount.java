89ac386131c1e4ae89d615adc0125006cc806fe5
Jin <jinyung7428@gmail.com>
2023-05-22 02:14:11 -0400

    first commit with construction error

--- src/bank/CheckingAccount.java
+++ src/bank/CheckingAccount.java
@@ -1,32 +1,32 @@
 package bank;
 
 public class CheckingAccount implements IAccount {
-  private double balance;
+    private double balance;
 
-  public CheckingAccount(double initialAmount) {
-    if (initialAmount < 0.01) {
-      throw new IllegalArgumentException("The starter amount must be greater than or equal to one cent.");
+    public CheckingAccount(double starterAmount) {
+        if (starterAmount < 0.01) {
+            throw new IllegalArgumentException("The starter amount has to be overed 1cent");
+        }
+
     }
-    this.balance = initialAmount;
-  }
 
-  @Override
-  public void deposit(double amount) {
+    @Override
+    public void deposit(double amount) {
 
-  }
+    }
 
-  @Override
-  public boolean withdraw(double amount) {
-    return false;
-  }
+    @Override
+    public boolean withdraw(double amount) {
+        return false;
+    }
 
-  @Override
-  public double getBalance() {
-    return 0;
-  }
+    @Override
+    public double getBalance() {
+        return 0;
+    }
 
-  @Override
-  public void performMonthlyMaintenance() {
+    @Override
+    public void performMonthlyMaintenance() {
 
-  }
+    }
 }
--- src/bank/IAccount.java
+++ src/bank/IAccount.java
@@ -6,8 +6,8 @@ package bank;
  */
 
 public interface IAccount {
-  void deposit(double amount);
-  boolean withdraw(double amount);
-  double getBalance();
-  void performMonthlyMaintenance();
+    void deposit(double amount);
+    boolean withdraw(double amount);
+    double getBalance();
+    void performMonthlyMaintenance();
 }
